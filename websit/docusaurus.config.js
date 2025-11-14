10aee651679b3ac7c88aa7916c5368f0d9ceb5e2
Jeff <bohuli2048@163.com>
2025-11-15 00:50:20 +0800

    tem save

--- websit/docusaurus.config.js
+++ websit/docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kumo-pub', // Usually your GitHub org/user name.
+  projectName: 'kmdo', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -97,7 +97,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/kumose/kmdo',
             label: 'GitHub',
             position: 'right',
           },
