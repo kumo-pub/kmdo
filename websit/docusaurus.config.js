66154ed3b78a8d6319c38bc5444f668cf08a9f5c
Jeff <bohuli2048@163.com>
2025-11-14 23:11:44 +0800

    init

--- websit/docusaurus.config.js
+++ websit/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://kmdo.kumose.cc',
+  url: 'https://kumo-pub.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'kmdo',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
