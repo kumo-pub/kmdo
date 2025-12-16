36db89f16dfa26582f547a0d9a4fbc0ee3e12d9d
Jeff <bohuli2048@163.com>
2025-12-17 00:57:11 +0800

    fix base url

---
+++ docs/zh-cn/assets/js/acecf23e.b717562c.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkwebsite=globalThis.webpackChunkwebsite||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/kmdo/zh-cn/blog","blogTitle":"Blog","authorsListPath":"/kmdo/zh-cn/blog/authors"}')}}]);
